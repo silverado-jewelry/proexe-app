0caf83489a3b2e80dff50610cebd0bae9d069243
oleksandr <oleksandr@appricotsoft.tech>
2024-09-15 22:13:52 +0200

    Add caching

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Movies\Services\MovieService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\StreamedJsonResponse;
 
 class MovieController extends Controller
